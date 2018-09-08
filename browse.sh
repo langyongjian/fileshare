2aef28cbc225e356fc6cfc2ee455a7b4f40ec480
lgcde <34829026+lgcde@users.noreply.github.com>
2018-09-08 16:40:09 +0800

    1

---
+++ browse.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p /www/caddy && cd /www/caddy && wget "https://caddyserver.com/download/linux/386?plugins=http.filemanager&license=personal&telemetry=on" -O caddy.tar.gz && tar -zxvf caddy.tar.gz caddy && chmod +x caddy && rm -rf caddy.tar.gz
+echo ":80 {
+#basicauth / username password #密码验证,删除注释生效
+#tls 你的邮箱地址@mail.com #tls证书自动申请,先解析到ip,然后开头的":80"改为"https://你的域名"删除注释生效
+ root /www/caddy #此处改成你要展示的路径
+ timeouts none
+ gzip
+ browse
+ filemanager /admin / {
+  database /www/caddy/filemanager.db
+  locale zh-cn }
+}
+" > /www/caddy/Caddyfile
+nohup /www/caddy/caddy  --conf=/www/caddy/Caddyfile -agree &
---
+++ h5ai.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+mkdir -p /www/caddy && cd /www/caddy && wget "https://caddyserver.com/download/linux/386?plugins=http.filemanager&license=personal&telemetry=on" -O caddy.tar.gz && tar -zxvf caddy.tar.gz caddy && chmod +x caddy && rm -rf caddy.tar.gz
+wget -P /www/h5ai https://release.larsjung.de/h5ai/h5ai-0.29.0.zip && unzip /www/h5ai/h5ai-0.29.0.zip -d /www/h5ai
+chmod +777 /www/h5ai/_h5ai/public/cache/ /www/h5ai/_h5ai/private/cache/
+git clone https://github.com/blueimp/jQuery-File-Upload.git "/www/jQuery-File-Upload"
+chmod +777 /www/jQuery-File-Upload/server/php/files/
+echo ":80 {
+#basicauth / username password #网页密码验证,删除注释生效
+#tls 你的邮箱地址@mail.com #tls证书自动申请,先解析到ip,然后开头的":80"改为"https://你的域名"删除注释生效
+ root /www/h5ai
+ timeouts none
+ gzip
+ filemanager /admin / {
+  database /www/caddy/filemanager.db
+  locale zh-cn }
+ fastcgi / /home/hhvm/sock php
+ rewrite {
+        if {path} ends_with /
+        to {dir}/index.html {dir}/index.php /_h5ai/public/index.php
+ }
+}
+:8080 {
+#basicauth / username password #网页密码验证,删除注释生效
+ root /www/jQuery-File-Upload/
+ timeouts none
+ gzip
+ fastcgi / /home/hhvm/sock php
+}
+" > /www/caddy/Caddyfile
